52c579dc7951d1fc5c2f80bac55ee72097215d3e
EraSela <es30857@seeu.edu.mk>
2026-06-03 10:59:15 +0200

    Improved full load procedure

--- SQL_Load_Methods/01_full_load.sql
+++ SQL_Load_Methods/01_full_load.sql
@@ -1,32 +1,55 @@
 CREATE OR ALTER PROCEDURE DW.FullLoad_SpotifyTracks
 AS
 BEGIN
-    DELETE FROM DW.SpotifyTracks;
+    SET NOCOUNT ON;
 
-    INSERT INTO DW.SpotifyTracks
-    (
-        TrackID,
-        Artists,
-        AlbumName,
-        TrackName,
-        Popularity,
-        DurationMs,
-        Explicit,
-        Danceability,
-        Energy,
-        TrackGenre
-    )
-    SELECT
-        track_id,
-        artists,
-        album_name,
-        track_name,
-        popularity,
-        duration_ms,
-        explicit,
-        danceability,
-        energy,
-        track_genre
-    FROM Landing.SpotifyTracks;
+    BEGIN TRY
+
+        DELETE FROM DW.SpotifyTracks;
+
+        INSERT INTO DW.SpotifyTracks
+        (
+            TrackID, Artists, AlbumName, TrackName,
+            Popularity, DurationMs, Explicit,
+            Danceability, Energy, TrackGenre
+        )
+        SELECT
+            track_id, artists, album_name, track_name,
+            popularity, duration_ms, explicit,
+            danceability, energy, track_genre
+        FROM Landing.SpotifyTracks;
+
+        INSERT INTO Config.LoadAudit
+        (
+            ProcedureName,
+            LoadType,
+            Status
+        )
+        VALUES
+        (
+            'DW.FullLoad_SpotifyTracks',
+            'Full Load',
+            'Success'
+        );
+
+    END TRY
+    BEGIN CATCH
+
+        INSERT INTO Config.LoadAudit
+        (
+            ProcedureName,
+            LoadType,
+            Status,
+            ErrorMessage
+        )
+        VALUES
+        (
+            'DW.FullLoad_SpotifyTracks',
+            'Full Load',
+            'Failed',
+            ERROR_MESSAGE()
+        );
+
+    END CATCH
 END;
 GO
